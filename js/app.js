115fa755050c7b3d831f97ff22413607b3ee65c8
alivasilchikova <Alina-vasilchikova@live.com>
2016-12-22 17:07:31 +0300

    First commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,36 @@
+var News = React.createClass({
+    render: function() {
+        return (
+            <div className="news">
+                К сожалению, новостей нет.
+            </div>
+        );
+    }
+});
+
+var Comment = React.createClass({
+    render: function() {
+        return (
+            <div className="comment">
+                Новостей нет, комментировать нечего.
+            </div>
+        );
+    }
+});
+
+var App = React.createClass({
+    render: function() {
+        return (
+            <div className="app">
+                Всем привет, я компонент App! Я умею отображать новости.
+                <News />
+                <Comment/>
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <App />,
     document.getElementById('root')
 );
